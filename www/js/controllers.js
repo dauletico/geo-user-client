dfab98d2c6485211966c550ed677778a069e538d
Jack Cable <jackhcable@gmail.com>
2018-02-17 13:26:59 -0700

    Fixed everything

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, ble, $ionicPlatform) {
+.controller('DashCtrl', function($scope, $ionicPlatform) {
   $ionicPlatform.ready(() => {
       $scope.deviceToken = '123454456456546';
       if (ble) {
