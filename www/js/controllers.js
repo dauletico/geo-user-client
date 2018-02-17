b9493618fc2684a5ede635a4dda07a577666b002
Jack Cable <jackhcable@gmail.com>
2018-02-17 16:02:22 -0700

    Update to buttons

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,47 +1,74 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $ionicPlatform) {
+  var discoveredDevice;
+  var uuid;
+
   $ionicPlatform.ready(() => {
       $scope.deviceToken = '123454456456546';
-      if (ble) {
-        ble.scan([], 5, (device) => {
-          // on success
-          if(device.advertising && device.advertising.kCBAdvDataServiceUUIDs && device.advertising.kCBAdvDataServiceUUIDs.length > 0) {
-            let uuid = device.advertising.kCBAdvDataServiceUUIDs[0];
-            // check if device is our peripheral
-            if(uuid.startsWith('15555555')) {
-              $scope.message = 'Got device with id: ' + device.id + '. Connecting...';
-              console.log(device.id);
-              ble.connect(device.id, (msg) => {
-                $scope.data = JSON.stringify(msg);
-                console.log('AAAAAAAAAAA')
-                console.log(msg)
-                for (var i = 0; i < msg.characteristics.length; i++) {
-                  let characteristic = msg.characteristics[i];
-                  if(characteristic.characteristic.startsWith('14444444')) {
-                    $scope.message += 'Now, writing to characteristic';
-                    console.log('WRITING');
-                    ble.write(device.id, uuid, characteristic.characteristic, stringToBytes('123454456456546'), (success) => {
-                      console.log('SUCCEEEEEDDDED')
-                    }, (failure) => {
-                      console.log('FAILUREE')
-                    });
-                  }
-                }
-              }, (err) => {
-                $scope.data = JSON.stringify(err);
-              });
-            }
-          }
-        }, (msg) => {
-          // Failure
-          $scope.message = JSON.stringify(msg);
-        });
-      } else {
-        $scope.message = 'BLE not available.';
+      $scope.scan = () => {
+        discoverDevice(ble, $scope);
+      };
+
+      $scope.connect = () => {
+        connectToDevice(ble, discoveredDevice, uuid, $scope);
       }
 
+      discoverDevice(ble, $scope);
   });
+
+  function discoverDevice(ble, $scope) {
+    var isScanDone = false;
+    if(!discoveredDevice) {
+      $scope.message = 'Searching for devices...'
+    }
+    ble.scan([], 3, (device) => {
+      // on success
+      if(device.advertising && device.advertising.kCBAdvDataServiceUUIDs && device.advertising.kCBAdvDataServiceUUIDs.length > 0) {
+        uuid = device.advertising.kCBAdvDataServiceUUIDs[0];
+        // check if device is our peripheral
+        if(isScanDone) return;
+        if(uuid.startsWith('15555555')) {
+          discoveredDevice = device;
+          $scope.message = 'Got device with id: ' + device.id + '. Would you like to conenct?';
+          isScanDone = true;
+        }
+      }
+    }, (msg) => {
+      // Failure
+      // $scope.message = JSON.stringify(msg);
+    });
+    
+  }
+
+
+  function connectToDevice(ble, device, uuid, $scope) {
+    ble.connect(device.id, (msg) => {
+      $scope.message = $scope.message + 'Connected...';
+      for (var i = 0; i < msg.characteristics.length; i++) {
+        let characteristic = msg.characteristics[i];
+        if(characteristic.characteristic.startsWith('14444444')) {
+          $scope.message = $scope.message + 'Now, writing to characteristic';
+          ble.write(device.id, uuid, characteristic.characteristic, stringToBytes('123454456456546'), (success) => {
+            console.log(success);
+            $scope.data = JSON.stringify(success);
+          }, (failure) => {
+          });
+        }
+      }
+    }, (err) => {
+      $scope.data = JSON.stringify(err);
+    });
+  }
+
+  function stringToBytes(string) {
+     var array = new Uint8Array(string.length);
+     for (var i = 0, l = string.length; i < l; i++) {
+         array[i] = string.charCodeAt(i);
+      }
+      return array.buffer;
+  }
+
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -68,12 +95,3 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 });
-
-function stringToBytes(string) {
-   var array = new Uint8Array(string.length);
-   for (var i = 0, l = string.length; i < l; i++) {
-       array[i] = string.charCodeAt(i);
-    }
-    return array.buffer;
-}
-
